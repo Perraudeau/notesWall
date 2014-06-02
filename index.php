6a9e9754f25335b139f41bd1f9b6b1f9aadb3062
Perraudeau Victor <perraudeau.victor@gmail.com>
2014-06-02 17:31:25 +0200

    DB connection+

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        include_once '/model/DBconnectModel.php';
-        foreach (SPDO::getInstance()->query('SELECT * FROM user') as $membre) {
-            echo ($membre[1]);
-        }
-        ?>
-    </body>
-</html>
+<?php
+session_start();
+
+?>
