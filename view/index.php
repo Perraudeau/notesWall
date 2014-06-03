ebd415c5f8245add95c13347f01f60303151e0a0
aslux6 <aslux6@LSC-BTS-1-B-016.lsc.local>
2014-06-03 17:15:24 +0200

    amélioration graphique

--- view/index.php
+++ view/index.php
@@ -1,20 +1,52 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
     <head>
-        <meta charset="utf-8">
-        <title>notesWall</title>
-        <meta name="description" content="TP PHP">
-        <meta name="author" content="Perraudeau Victor & Leroux Alexis">
+	<meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <title>Notes Wall</title>
+       
+      
+        <meta name="author" content="Perraudeau Victor & Leroux Alexis" />
+        <link rel="shortcut icon" href="../favicon.ico"> 
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+        <script src="js/modernizr.custom.63321.js"></script>
+		<!--[if lte IE 7]><style>.main{display:none;} .support-note .note-ie{display:block;}</style><![endif]-->
     </head>
     <body>
         <div class="container">
-            <form class="form-signin">
-                <h2 class="form-signin-heading">Connectez vous</h2>
-                <input type="text" class="input-block-level" placeholder="Adresse Email">
-                <input type="password" class="input-block-level" placeholder="Mot de passe">
-                <button class="btn btn-large btn-primary" type="submit">Connexion</button>
-                <a href="./newUser.php">Nouvel utilisateur</a>
-            </form>
+				
+			<header>
+			
+				<h1>Notes <strong>Wall</strong></h1>
+				<div class="support-note">
+					<span class="note-ie">Sorry, only modern browsers.</span>
+				</div>
+			</header>
+			
+			<section class="main">
+				<form class="form-1" >
+					<p class="field">
+						<input type="text" name="login" placeholder="Email">
+						<i class="icon-user icon-large"></i>
+					</p>
+						<p class="field">
+							<input type="password" name="password" placeholder="Password">
+							<i class="icon-lock icon-large"></i>
+					</p>
+					<p class="submit">
+						<button type="submit" name="submit"><i class="icon-arrow-right icon-large"></i></button>
+					</p>
+                                        
+                                        <br>
+                                        <p class="register">
+                                            <button type="button" name="register"><a href="./newUser.php"> Nouvel utilisateur</a></button>
+                                        
+                                        </p>
+                                        
+				</form>
+             
+			</section>
         </div>
     </body>
 </html>
