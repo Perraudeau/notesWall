827b011ddd5126270f97e3a634ed7e689f51254b
aslux6 <aslux6@LSC-BTS-1-B-016.lsc.local>
2014-06-11 15:32:47 +0200

    modification git 3

--- view/index.php
+++ view/index.php
@@ -3,7 +3,7 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="fr">
-    <head>
+<head>
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
         <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
@@ -13,8 +13,20 @@ session_start();
         <meta name="author" content="Perraudeau Victor & Leroux Alexis" />
         <link href="./css/style.css" rel="stylesheet">
         <link href="./css/metro-bootstrap.css" rel="stylesheet">
-        <script src="js/modernizr.custom.63321.js"></script>
-                <!--[if lte IE 7]><style>.main{display:none;} .support-note .note-ie{display:block;}</style><![endif]-->
+        <!-- Load JavaScript Libraries -->
+        <script src="./js/jquery/jquery.min.js"></script>
+        <script src="./js/jquery/jquery.widget.min.js"></script>
+        <script src="./js/jquery/jquery.mousewheel.js"></script>
+        <script src="./js/prettify/prettify.js"></script>
+        <script src="./js/holder/holder.js"></script>
+
+        <!-- Metro UI CSS JavaScript plugins -->
+        <script src="./js/load-metro.js"></script>
+
+        <!-- Local JavaScript -->
+        <script src="./js/docs.js"></script>
+        <script src="./js/github.info.js"></script>
+
     </head>
     <body class="metro">
         <header class="bg-dark" data-load="header.html"></header>
