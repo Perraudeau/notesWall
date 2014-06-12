b269e932e8dfafcf9286a9a53e468e41236fb3c8
Perraudeau Victor <perraudeau.victor@gmail.com>
2014-06-12 15:28:17 +0200

    commencement notesWall

---
+++ handler/notesWallHandler.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Description of notesWallHandler
+ *
+ * @author Perraudeau
+ */
+class notesWallHandler {
+
+//Menu du haut
+    public function headerNotesWall() {
+        
+    }
+
+//barre pour permettre la creation d'une note rapide
+    public function newNotesWallBar() {
+        
+    }
+
+//affichage des notes
+    public function container() {
+        
+    }
+
+//bas de page
+    public function footer() {
+        
+    }
+
+}
