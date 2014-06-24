f43290e3c1a9b39d58de40ddfb80955eb8abc2c4
Perraudeau Victor <perraudeau.victor@gmail.com>
2014-06-24 17:30:13 +0200

    date + ajax

--- handler/notesWallContainer.php
+++ handler/notesWallContainer.php
@@ -40,7 +40,7 @@ class notesWallContainer {
                     <li align="center"><h1><?php echo $result[$index]['title'] ?></h1></li>
                     <li align="center"><?php echo noteWallModel::getEmailUser($result[$index]['idUser']) ?></li><br>
                     <li align="justify"><?php echo $result[$index]['text'] ?></li><br><br>
-                    <li><?php echo notesWallContainer::timestampTofr($result[$index]['alarmDate']) ?></li>
+                    <li><?php echo notesWallContainer::UsTofr($result[$index]['alarmDate']) ?></li>
                     <li align="right"><?php echo $result[$index]['place'] ?></li>
                 </div>
                 <br>
@@ -49,16 +49,12 @@ class notesWallContainer {
         <?php
     }
 
-    public static function timestampToFr($date) {
+    public static function UsToFr($date) {
         return 'Le ' . date('d/m/Y à H:i', strtotime(str_replace('-', '/', $date)));
     }
 
-    public static function FrToTimestamp($date) {
-       $date = str_replace(array(' ', ':'), '-', $date);
-        list($hour,$minute,$second,$day,$month,$year) = explode('-', $date);
-        $timestamp = mktime($hour,$minute,$second,$month, $day, $year);
-        return $timestamp;
+    public static function FrToUs($date) {
+        return date('Y-m-d H:i:00', strtotime(str_replace('/', '-', $date)));
     }
-
 }
 ?>
